dd74166a7619a28b374ee65329a6135d5df622bb
henricomaeda <henrico.maeda@gmail.com>
2023-05-28 15:20:04 -0300

    Defined default components

---
+++ src/screens/FormScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import * as React from "react";
+
+const FormScreen = () => {
+    return (
+        <View style={{ flex: 1 }} />
+    )
+};
+
+export default FormScreen;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import * as React from "react";
+
+const HomeScreen = () => {
+    return (
+        <View style={{ flex: 1 }} />
+    )
+};
+
+export default HomeScreen;
