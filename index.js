f93cd8a695887b590c9e6e4d7d631003b011ef5f
henricomaeda <henrico.maeda@gmail.com>
2023-05-28 15:19:39 -0300

    Added more comments and a docstring

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 /**
+ * ChromoCal Mobile App
+ *
+ * Entry point for the ChromoCal mobile application. It registers the main
+ * component with the React Native AppRegistry for navigation purposes.
+ *
  * @format
+ * @flow
+ * @providesModule ChromoCal
+ * @flow strict-local
+ * @module ChromoCal
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// Import the application's name and main component.
+import { name as appName } from './app.json';
+import { AppRegistry } from 'react-native';
+import App from './src/App';
 
+// Register the navigation component.
 AppRegistry.registerComponent(appName, () => App);
