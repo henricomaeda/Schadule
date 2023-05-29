d5f81483c8089fc5a9a3f748a7a9640c052060d8
henricomaeda <henrico.maeda@gmail.com>
2023-05-29 01:18:58 -0300

    Added functions to format date and time

--- src/utils/Functions.js
+++ src/utils/Functions.js
@@ -1,10 +1,13 @@
+// Import required global variables.
+import { globals } from "../Globals";
+
 /**
- * Generates a unique ID based on the given data.
+ * Generate a unique ID based on the given data.
  *
  * @param {Array} data - An array of objects containing ID properties.
  * @returns {number} - A unique ID that doesn't exist in the data.
  */
-export const generateUniqueId = (data) => {
+export const generateUniqueId = data => {
     // Create a set of existing IDs from the data
     const idSet = new Set(data.map(item => item.id));
 
@@ -13,4 +16,26 @@ export const generateUniqueId = (data) => {
 
     // Return the unique ID
     return id;
+};
+
+/**
+ * Format the time to ensure it has leading zero if less than 10.
+ * @param {number} time - The time to format.
+ * @returns {string} - The formatted time.
+ */
+export const formatTime = time => time < 10 ? `0${time}` : `${time}`;
+
+/**
+ * Format the date in a specific format.
+ * @param {Date} date - The date to format.
+ * @returns {string} - The formatted date.
+ */
+export const formatDate = (date, short = true) => {
+    const day = date.getDate();
+    const month = globals.months[date.getMonth()].toLowerCase();
+    if (short) {
+        const weekDay = globals.weekDays[date.getDay()];
+        return `${weekDay}, ${day} de ${month}`;
+    }
+    else return `${day} de ${month} de ${date.getFullYear()}`;
 };
