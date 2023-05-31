fcd4d2c76ed010bf175e31d694fe2374550ca3ef
henricomaeda <henrico.maeda@gmail.com>
2023-05-30 22:19:20 -0300

    Added new components and more options

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import { TouchableOpacity } from "react-native";
-import React from "react";
+import * as React from "react";
 
 // Import the components to render, and global variables.
-import HomeScreen from "./screens/HomeScreen";
+import DataScreen from "./screens/DataScreen";
+import DetailsScreen from "./screens/DetailsScreen";
 import FormScreen from "./screens/FormScreen";
+import HomeScreen from "./screens/HomeScreen";
 import { globals } from "./Globals";
 
 // Import the new functions to navigate and define the navigation theme.
@@ -23,9 +25,11 @@ const NavigationTheme = {
 // Create a native stack navigator component.
 const Stack = createNativeStackNavigator();
 export default function App() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   return (
     <NavigationContainer theme={NavigationTheme}>
       <Stack.Navigator
+        initialRouteName="HomeScreen"
         screenOptions={({ navigation, route }) => ({
           headerTitle: globals.app.name,
           headerTintColor: globals.colors.placeholder,
@@ -43,17 +47,44 @@ export default function App() {
             </TouchableOpacity>
           ),
           headerRight: () => route.name === "HomeScreen" && (
-            <TouchableOpacity onPress={() => replaceRoute(navigation, route.name)}>
-              <Icon
-                color={globals.colors.placeholder}
-                size={globals.app.width / 18}
-                name="cached"
-              />
+            <TouchableOpacity
+              activeOpacity={1}
+              onPress={() => setIsMenuOpen(false)}
+              style={{
+                height: globals.app.width / 10,
+                justifyContent: "flex-end",
+                flexDirection: "row",
+                alignItems: "center",
+                flex: 1,
+              }}>
+              <TouchableOpacity
+                onPress={() => {
+                  setIsMenuOpen(false);
+                  setTimeout((() => replaceRoute(navigation, route.name)), 0);
+                }}
+                style={{ marginRight: globals.app.width / 42 }}>
+                <Icon
+                  color={globals.colors.placeholder}
+                  size={globals.app.width / 18}
+                  name="cached"
+                />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => setIsMenuOpen(!isMenuOpen)}>
+                <Icon
+                  color={globals.colors.placeholder}
+                  size={globals.app.width / 18}
+                  name="more-vert"
+                />
+              </TouchableOpacity>
             </TouchableOpacity>
           )
         })}>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="DataScreen" component={DataScreen} />
+        <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
         <Stack.Screen name="FormScreen" component={FormScreen} />
+        <Stack.Screen name="HomeScreen">
+          {(props) => <HomeScreen {...props} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer >
   );
